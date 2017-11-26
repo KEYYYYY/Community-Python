f4b00a4a4d20736c3f64d9cf772095bd845aa774
Z <763997136@qq.com>
2017-11-26 13:37:02 +0800

    清空原来项目，改用django实现

--- manage.py
+++ manage.py
@@ -1,28 +1,22 @@
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+#!/usr/bin/env python
+import os
+import sys
 
-from app import create_app, db
-
-app = create_app()
-manager = Manager(app)
-migrate = Migrate(app, db)
-
-manager.add_command('db', MigrateCommand)
-
-
-@manager.shell
-def make_shell_context():
-    from app.auth.models import User
-    from app.home.models import Article, Follow, Comment
-    return dict(
-        app=app,
-        db=db,
-        User=User,
-        Article=Article,
-        Follow=Follow,
-        Comment=Comment
-    )
-
-
-if __name__ == '__main__':
-    manager.run()
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "community.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
