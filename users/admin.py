bdd00fc879c4ed7eab3bdb8c5115dfa1b769a0ec
Z <763997136@qq.com>
2017-11-26 14:06:50 +0800

    增加文章模型

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogArticle
+
+
+class BlogArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish_time')
+    list_filter = ('title', 'publish_time')
+    search_fields = ('title', 'body')
+
+
+admin.site.register(BlogArticle, BlogArticleAdmin)
